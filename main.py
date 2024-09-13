a11557fc865520a1513bd87845bb9d72330dbc21
Pascal Iwohn <pascal.iwohn@bizerba.com>
2024-09-13 12:10:55 +0200

    Added src path to main file

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import sys
+import os
+
+# Get the absolute path of the src directory
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'src'))
+
+# Add the src directory to sys.path
+sys.path.insert(0, src_path)
+
+
 from cli_menu.user_selection import user_menu_main
 from data_storage.json import JsonStorageInterface
 
